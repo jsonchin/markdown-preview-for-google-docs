0f651e8a4a7d61893d9868a465a87b998543a5a9
TR4Q <jlchin@berkeley.edu>
2018-04-15 21:58:31 -0700

    Switches to github css and adds code highlighting.

--- src/render.js
+++ src/render.js
@@ -24,7 +24,21 @@ function convertToMarkdownHtml(markdown) {
 
 function getConverter() {
   if (converter == null) {
-    converter = new showdown.Converter();
+    showdown.extension('prettify', function () {
+        return [{
+            type: 'output',
+            filter: function (source) {
+                return source.replace(/(<pre[^>]*>)?[\n\s]?<code([^>]*)>/gi, function (match, pre, codeClass) {
+                    if (pre) {
+                        return '<pre class="prettyprint linenums"><code' + codeClass + '>';
+                    } else {
+                        return ' <code class="prettyprint">';
+                    }
+                });
+            }
+        }];
+    });
+    converter = new showdown.Converter({extensions: ['prettify']});
     converter.setFlavor('github');
   }
   return converter;
